0dc607e6ddfda67ed6663c41ab0205605583626e
didemdemir11 <kdidemdemir@gmail.com>
2024-09-11 17:44:32 +0300

    complete cypress test for IT1

--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -11,5 +11,34 @@ describe('Pizza SiparisFormu Testleri', () => {
     
     });
   });
-
+describe('Sipariş formu testleri', () => {
+  it('İsim alanına metin girmeli ve kontrol etmeli',()=>{
+    cy.visit('http://localhost:5173/siparis-formu'); 
+    cy.get('input[name="name"]').type('Didem Demir');
+    cy.get('input[name="name"]').should('have.value','Didem Demir');  
+  });
+  it('Birden fazla malzeme seçmeli',()=>{
+    cy.visit('http://localhost:5173/siparis-formu'); 
+    cy.get('input[name="toppings"]').check(['Pepperoni','Sucuk','Kabak','Mısır']);
+    cy.get('input[name="toppings"]').each(($checkbox)=> {
+      if($checkbox.prop('checked')) {
+        cy.wrap($checkbox).should('be.checked');
+      }
+    })
+  });
+  it('Formu seçimleri yapıp butonla göndermeli',()=> {
+    cy.visit('http://localhost:5173/siparis-formu'); 
+    cy.get('input[name="name"]').type('Didem Demir');
+    cy.get('input[type="radio"][value="Büyük"]').check();
+    cy.get('select[name="thickness"]').select('İnce Hamur');
+    cy.get('input[name="toppings"]').check(['Pepperoni','Sucuk','Kabak','Mısır']);
+    cy.get('input[name="quantity"]').clear().type('2');
+    cy.get('button[type="submit"]').should('not.be.disabled').click();
+    cy.url().should('include','/siparis-onay')
+  });
+  it('Sipariş butonu ilk başta disabled olmalı', ()=>{
+    cy.visit('http://localhost:5173/siparis-formu'); 
+    cy.get('button[type="submit"]').should('be.disabled');
+  })
+})
  
